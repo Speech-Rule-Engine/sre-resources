50712f30b13a59c23d265fb3265e45d2aa35e41d
zorkow <volker.sorge@gmail.com>
2020-03-05 00:19:14 +0000

    Test replacement for SRE.

--- tools/replacement.el
+++ tools/replacement.el
@@ -1,3 +1,62 @@
+(defun sre-replace-test (style position)
+  (interactive "M" "n")
+  (search-forward style)
+  (search-backward "''")
+  (forward-char 1)
+  (other-window 1)
+  (search-forward "<tr>")
+  (search-forward "<td>" nil nil position)
+  (let ((start (point))
+        (end (progn
+               (search-forward "</td>")
+               (backward-char 5)
+               (point)
+               )))
+    (copy-region-as-kill start end))
+  (search-forward "</tr>")
+  (other-window 1)
+  (yank)
+  (search-forward style)
+  )
+
+
+(defun sre-replace-all-test (style position)
+  (interactive)
+  (let ((result t))
+    (while result
+      (condition-case ex
+          (sre-replace-test style position)
+          ('error (setq result nil))))
+    )
+  )
+
+
+(defun sre-remove-test-expected ()
+  (interactive)
+  (search-forward "this.execute")
+  (search-forward ", '")
+  (print "0")
+  (print (point))
+  (let ((start (point))
+        (end (progn
+               (search-forward "'")
+               (1- (point)))))
+    (print start)
+    (print end)
+    (delete-region start end)
+    )
+  )
+
+(defun sre-remove-all-test-expected ()
+  (interactive)
+  (let ((result t))
+    (while result
+      (condition-case ex
+          (sre-remove-test-expected)
+          ('error (setq result nil))))
+    )
+  )
+
 (fset 'remove-test-expected
    [?\C-s ?, ?  ?\' left right ?\C-  ?\C-s ?\' left ?\C-w ?\C-e right])
 
