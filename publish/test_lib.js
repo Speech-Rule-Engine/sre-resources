6119a27ba448a198039ba915189ac54b45dc9af0
zorkow <volker.sorge@gmail.com>
2020-05-08 15:10:55 +0100

    Library version of browser test.

---
+++ publish/test_lib.js
@@ -0,0 +1,48 @@
+let sreTest = {
+
+  setup: function(features) {
+    SRE.setupEngine(features);
+    let count = 0;
+    return new Promise((res, rej) => {
+      function checkSRE() {
+        if (SRE && SRE.engineReady()) {
+          res();
+        } else if (count < 30) {
+          count++;
+          setTimeout(checkSRE, 200);
+        } else {
+          rej();
+        }
+      }
+      checkSRE();
+    });
+  },
+  
+  test: function(id) {
+    var xmls = new sre.SystemExternal.xmldom.XMLSerializer();
+    var test = xmls.serializeToString(document.getElementById(id));
+    // Edge takes too long to render. So a little timeout!
+    let promise = new Promise((res) => {
+      setTimeout(() => {
+        res(SRE.toSpeech(test));}, 200);
+    });
+    return promise.then((x) => {console.log(x);});
+
+  },
+
+
+  runTests: function(features = {}) {
+    let promise = sreTest.setup(features);
+    return promise.then(() => {return sreTest.test('test1');})
+      .then(() => {return sreTest.test('test2');})
+      .then(() => {return sreTest.test('test3');});
+  },
+
+  runAllTests: function() {
+    sreTest.runTests().
+      then(() => {return sreTest.runTests({locale: 'fr'});}).
+      then(() => {return sreTest.runTests({locale: 'es', domain: 'mathspeak', style: 'default'});}).
+      then(() => {return sreTest.runTests({locale: 'nemeth', domain: 'default', modality: 'braille'});});
+  }
+  
+};
