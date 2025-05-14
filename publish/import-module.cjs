d58502534100bd70b24ede915d0481446caf87af
zorkow <volker.sorge@gmail.com>
2025-05-14 17:25:50 +0200

    update import files to use explicit options

--- publish/import-module.cjs
+++ publish/import-module.cjs
@@ -5,9 +5,10 @@ console.log(SystemExternal.fs);
 
 
 let Semantic = require('./node_modules/speech-rule-engine/cjs/semantic_tree/semantic.js');
+let { Options } = require('./node_modules/speech-rule-engine/cjs/common/options.js');
 
 console.log(Semantic);
-let quadratic = Semantic.getTreeFromString('<math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mo>&#x2212;</mo><mi>b</mi><mo>&#xB1;</mo><msqrt><mrow><msup><mi>b</mi><mn>2</mn></msup><mo>&#x2212;</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></mrow></math>');
+let quadratic = Semantic.getTreeFromString('<math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mo>&#x2212;</mo><mi>b</mi><mo>&#xB1;</mo><msqrt><mrow><msup><mi>b</mi><mn>2</mn></msup><mo>&#x2212;</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></mrow></math>', new Options());
 console.log(quadratic.toString());
 
 let System = require('./node_modules/speech-rule-engine/cjs/common/system.js');
--- publish/import-module.mjs
+++ publish/import-module.mjs
@@ -11,9 +11,10 @@ console.log(SystemExternal.extRequire('fs'));
 
 
 import * as Semantic from './node_modules/speech-rule-engine/js/semantic_tree/semantic.js';
+import { Options } from './node_modules/speech-rule-engine/js/common/options.js';
 
 console.log(Semantic);
-let quadratic = Semantic.getTreeFromString('<math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mo>&#x2212;</mo><mi>b</mi><mo>&#xB1;</mo><msqrt><mrow><msup><mi>b</mi><mn>2</mn></msup><mo>&#x2212;</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></mrow></math>');
+let quadratic = Semantic.getTreeFromString('<math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><mrow><mi>x</mi><mo>=</mo><mfrac><mrow><mo>&#x2212;</mo><mi>b</mi><mo>&#xB1;</mo><msqrt><mrow><msup><mi>b</mi><mn>2</mn></msup><mo>&#x2212;</mo><mn>4</mn><mi>a</mi><mi>c</mi></mrow></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></mrow></math>', new Options());
 console.log(quadratic.toString());
 
 import * as System from  './node_modules/speech-rule-engine/js/common/system.js';
